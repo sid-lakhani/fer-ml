49ba5ecaa05d11e72c283754dc7c027b42a91f7c
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-10-26 13:20:47 +0530

    revert:

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import cv2
 import os
 import numpy as np
-import time
 from sklearn.neighbors import KNeighborsClassifier
 
 train_path = '../dataset/train/'
@@ -25,51 +24,40 @@ def load_data():
     return np.array(data), np.array(labels)
 
 X, y = load_data()
+
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X, y)
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('../utils/haarcascade_frontalface_default.xml')
 
-fps_limit = 15
-prev_time = 0
-frame_count = 0
-
 while True:
-    time_elapsed = time.time() - prev_time
-    if time_elapsed > 1.0 / fps_limit:
-        prev_time = time.time()
-        _, frame = cap.read()
-        frame = cv2.resize(frame, (320, 240))
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, frame = cap.read()
+    frame = cv2.resize(frame, (640, 480))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        if frame_count % 5 == 0:
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        frame_count += 1
+    for x, y, w, h in faces:
+        face_roi = gray[y:y + h, x:x + w]
+        face_resized = cv2.resize(face_roi, (48, 48))
+        face_flattened = face_resized.flatten().reshape(1, -1)
+        emotion_index = knn.predict(face_flattened)[0]
+        emotion = emotion_labels[emotion_index]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
-        for x, y, w, h in faces:
-            face_roi = gray[y:y + h, x:x + w]
-            face_resized = cv2.resize(face_roi, (48, 48))
-            face_flattened = face_resized.flatten().reshape(1, -1)
+    cv2.imshow('Emotion Detection', frame)
 
-            emotion_index = knn.predict(face_flattened)[0]
-            emotion = emotion_labels[emotion_index]
-
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-
-        cv2.imshow('Emotion Detection', frame)
-
-        key = cv2.waitKey(10)
-        if key == ord('q'):
-            break
-        elif key == 32 and len(faces) > 0:
-            emotion_counters[emotion] += 1
-            save_folder = os.path.join(emotions_path, emotion)
-            os.makedirs(save_folder, exist_ok=True)
-            file_name = f'{emotion}-{emotion_counters[emotion]}.png'
-            cv2.imwrite(os.path.join(save_folder, file_name), frame)
-            print(f"Picture saved in {save_folder}/{file_name}")
+    key = cv2.waitKey(10)
+    if key == ord('q'):
+        break
+    elif key == 32 and len(faces) > 0:
+        emotion_counters[emotion] += 1
+        save_folder = os.path.join(emotions_path, emotion)
+        os.makedirs(save_folder, exist_ok=True)
+        file_name = f'{emotion}-{emotion_counters[emotion]}.png'
+        cv2.imwrite(os.path.join(save_folder, file_name), frame)
+        print(f"Picture saved in {save_folder}/{file_name}")
 
 cap.release()
 cv2.destroyAllWindows()
