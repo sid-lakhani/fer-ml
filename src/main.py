87aaf9b9f4635e5a3b7a5ef10ab52dc15aa0db73
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-11-04 11:28:06 +0530

    feat: pre-trained model
    added option to train a new model or use a pre-trained model

--- src/main.py
+++ src/main.py
@@ -6,44 +6,55 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
+model_path = '../models/emotion_cnn_model.h5'
 train_path = '../dataset/train/'
 emotions_path = '../emotions'
+face_cascade_path = '../utils/haarcascade_frontalface_default.xml'
 
 emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 emotion_counters = {emotion: 0 for emotion in emotion_labels}
 
-def load_data():
-    data = []
-    labels = []
-    for emotion in emotion_labels:
-        folder = os.path.join(train_path, emotion)
-        for file in os.listdir(folder):
-            img_path = os.path.join(folder, file)
-            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-            if img is not None:
-                img_resized = cv2.resize(img, (48, 48))
-                data.append(img_resized)
-                labels.append(emotion_labels.index(emotion))
-    return np.array(data), np.array(labels)
+use_pretrained = input("Do you want to use the pretrained model? (yes/no): ").strip().lower()
 
-X, y = load_data()
-X = X.reshape(-1, 48, 48, 1) / 255.0
+if use_pretrained == 'yes' and os.path.exists(model_path):
+    cnn_model = tf.keras.models.load_model(model_path)
+    print(f"Loaded pretrained model from {model_path}")
+else:
+    def load_data():
+        data = []
+        labels = []
+        for emotion in emotion_labels:
+            folder = os.path.join(train_path, emotion)
+            for file in os.listdir(folder):
+                img_path = os.path.join(folder, file)
+                img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                if img is not None:
+                    img_resized = cv2.resize(img, (48, 48))
+                    data.append(img_resized)
+                    labels.append(emotion_labels.index(emotion))
+        return np.array(data), np.array(labels)
 
-cnn_model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
-    MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dense(len(emotion_labels), activation='softmax')
-])
+    X, y = load_data()
+    X = X.reshape(-1, 48, 48, 1) / 255.0
 
-cnn_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-cnn_model.fit(X, y, epochs=10, batch_size=32)
+    cnn_model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
+        MaxPooling2D(2, 2),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(len(emotion_labels), activation='softmax')
+    ])
+
+    cnn_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    cnn_model.fit(X, y, epochs=10, batch_size=32)
+    
+    cnn_model.save(model_path)
+    print(f"Model trained and saved to {model_path}")
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('../utils/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 while True:
     _, frame = cap.read()
